05058094a220031123381cc9a4ab9509695dff77
Aaron <aaron@Mav3r1ck.com>
2015-04-18 14:52:43 -0400

    Added Search Bar

--- PhotoSearchExample/ViewController.swift
+++ PhotoSearchExample/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchInstagramByHashtag("iOS")
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Utility methods
+    func searchInstagramByHashtag(searchString:String) {
+        let manager = AFHTTPRequestOperationManager()
+        manager.GET( "https://api.instagram.com/v1/tags/\(searchString)/media/recent?client_id=2cafe42dfff64c6c81a4018a7ad1e195",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                if let dataArray = responseObject["data"] as? [AnyObject] {
+                    var urlArray:[String] = []
+                    for dataObject in dataArray {
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                            urlArray.append(imageURLString)
+                        }
+                    }
+                    
+                    //ScrollView Code
+                    self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                    let imageWidth = self.view.frame.width
+                    self.scrollView.contentSize = CGSizeMake(imageWidth, imageWidth * CGFloat(dataArray.count))
+                    for var i = 0; i < urlArray.count; i++ {
+                        let imageView = UIImageView(frame: CGRectMake(0, imageWidth*CGFloat(i), imageWidth, imageWidth))
+                        imageView.setImageWithURL( NSURL(string: urlArray[i]))
+                        self.scrollView.addSubview(imageView)
+                    }
+                }
+            },
+            
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+        
     }
-
-
+    //MARK: UISearchBarDelegate protocol methods
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        searchBar.resignFirstResponder()
+        searchInstagramByHashtag(searchBar.text)
+        
+    }
 }
-
---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  PhotoSearchExample
+//
+//  Created by Aaron Ackerman on 4/18/15.
+//  Copyright (c) 2015 Aaron Ackerman. All rights reserved.
+//
+
+#import "AFNetworking/AFNetworking.h"
+#import "UIKit+AFNetworking/UIKit+AFNetworking.h"
+#ifndef PhotoSearchExample_bridge_h
+#define PhotoSearchExample_bridge_h
+
+
+#endif
